e73f48376e4f38c56b550507518195c3e6877db8
Gabreta <vmgabriel96@gmail.com>
2018-01-22 21:26:31 -0500

    Agregando HttpwebpackPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,12 @@
-/*
-    ./webpack.config.js
-*/
+//Dependencias
 const path = require('path');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPluginConfig = new HtmlWebpack({
+  template: './client/index.html',
+  filename: 'index.html',
+  inyect: 'body'
+});
 
 module.exports = {
   entry: './client/index.js',
@@ -15,5 +20,6 @@ module.exports = {
 	{ test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ },
 	{ test: /\.scss$/, loader: 'sass-loader', exclude: /node_modules/ }
     ]
-  }
+  },
+  plugins: [HtmlWebpackPluginConfig]
 }
